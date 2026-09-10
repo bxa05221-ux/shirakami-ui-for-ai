ad04386474cc78df84f539d79c4ad4cf2e02b609
bxa05221-ux <bxa05221@gmail.com>
2026-09-10 17:44:57 +0900

    R0001: add observation boundary primitives

---
+++ src/shirakami_ui_for_ai/__init__.py
@@ -0,0 +1,5 @@
+"""UI for AI observation-boundary primitives."""
+
+from .observation import ObservationContext, ObservationResult, ObservationRecord
+
+__all__ = ["ObservationContext", "ObservationResult", "ObservationRecord"]
